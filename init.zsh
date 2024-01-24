917aac7d827b56b22a1ce77dc4c945c69ba72408
Ota Mares <1375307+omares@users.noreply.github.com>
2024-01-24 17:42:29 +0100

    Add direnv and mcfly

--- init.zsh
+++ init.zsh
@@ -1,16 +1,26 @@
+# supress asdf direnv output
+export DIRENV_LOG_FORMAT=""
+
+source <(direnv hook zsh)
+
+
 # use nvim
 alias vi="nvim"
 alias vim="nvim"
 export EDITOR="nvim"
 
-# supress asdf direnv output
-export DIRENV_LOG_FORMAT=""
-
+#PATH="$PATH:~/.asdf/bin"
 # Use bat
 alias cat='bat'
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# mcfly
+#export MCFLY_KEY_SCHEME=vim
+export MCFLY_FUZZY=2
+export MCFLY_INTERFACE_VIEW=BOTTOM
+export MCFLY_PROMPT="❯"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
